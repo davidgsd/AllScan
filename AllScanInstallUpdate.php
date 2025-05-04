0ec4a935152678718fa7c466ae21f5cdb6a15e56
David Gleason <david@allscan.info>
2025-05-04 01:16:23 -0700

    Optimizations to installer/updater to reduce unnecessary messages and better check system status prior to running potentially unnecessary commands. Prompt user to check and copy DTMF command support files to Asterisk agi-bin folder.

--- AllScanInstallUpdate.php
+++ AllScanInstallUpdate.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php
-$AllScanInstallerUpdaterVersion = "v1.24";
+$AllScanInstallerUpdaterVersion = "v1.25";
 define('NL', "\n");
 // Execute this script by running "sudo ./AllScanInstallUpdate.php" from any directory. We'll then determine
 // the location of the web root folder, cd to that folder, check if you have AllScan installed and install
@@ -54,6 +54,7 @@ if($cwd !== $webdir) {
 $asdir = 'allscan';
 // Enable mkdir(..., 0775) to work properly
 umask(0002);
+clearstatcache();
 
 // Check if dir exists. If so, see if an update is needed. If not, install AllScan
 $dlfiles = true;
@@ -133,8 +134,10 @@ if($dlfiles) {
 }
 
 msg("Verifying $asdir dir has $group group writable permissions");
-execCmd("chmod 775 $asdir");
-execCmd("chgrp $group $asdir");
+if((fileperms($asdir) & 0777) != 0775)
+	execCmd("chmod 775 $asdir");
+if(getGroupName($asdir) !== $group)
+	execCmd("chgrp $group $asdir");
 execCmd("chmod 664 $asdir/*.ini");
 execCmd("chgrp $group $asdir/*.ini");
 
@@ -147,14 +150,14 @@ msg("PHP Version: " . phpversion());
 // Confirm necessary php extensions are installed
 msg("Checking OS packages and PHP extensions...");
 if(!class_exists('SQLite3')) {
-	msg("NOTE: Required SQLite3 Class not found." . NL
+	msg("Required SQLite3 Class not found." . NL
 		."Try running the update commands below to update your OS and php-sqlite3 package." . NL
 		."You may also need to enable the pdo_sqlite and sqlite3 extensions in php.ini.");
 }
 
 msg("Ready to run OS update/upgrade commands." . NL
 	."If you have recently updated and upgraded your system you do not need to do this now." . NL
-	."NOTE: THE UPDATE/UPGRADE PROCESS CAN POTENTIALLY CAUSE OLDER SOFTWARE PACKAGES TO STOP WORKING." . NL
+	."THE UPDATE/UPGRADE PROCESS CAN POTENTIALLY CAUSE OLDER SOFTWARE PACKAGES TO STOP WORKING." . NL
 	."DO NOT PROCEED WITH THIS STEP IF YOU ARE NOT SURE OR IF YOU ARE NOT AN AUTHORIZED SERVER ADMIN." . NL
 	."Otherwise it is recommended to ensure your system is up-to-date.");
 $s = readline("Enter 'y' to proceed, any other key to skip this step: ");
@@ -189,18 +192,24 @@ if(is_executable('/usr/bin/apt')) {
 		passthruCmd("sudo apt install -y avahi-daemon 2> /dev/null");
 	}
 	sleep(1);
-}
-if(is_file('/etc/systemd/system/asl3-update-astdb.service')) {
-	passthruCmd("systemctl enable asl3-update-astdb.service 2> /dev/null");
-	passthruCmd("systemctl enable asl3-update-astdb.timer 2> /dev/null");
-	passthruCmd("systemctl start asl3-update-astdb.timer 2> /dev/null");
-	// Make a readable copy of allmon3.ini (Allmon3 updates can reset the file permissions)
-	$fname = '/etc/allmon3/allmon3.ini';
-	if(file_exists($fname)) {
-		$fname2 = '/etc/asterisk/allmon.ini.php';
-		execCmd("cp $fname $fname2");
-		execCmd("chmod 660 $fname2");
-		execCmd("chgrp $group $fname2");
+	if(is_file('/etc/systemd/system/asl3-update-astdb.service')) {
+		if(exec('systemctl is-enabled asl3-update-astdb.service') !== "enabled")
+			passthruCmd("systemctl enable asl3-update-astdb.service 2> /dev/null");
+		if(exec('systemctl is-enabled asl3-update-astdb.timer') !== "enabled") {
+			passthruCmd("systemctl enable asl3-update-astdb.timer 2> /dev/null");
+			passthruCmd("systemctl start asl3-update-astdb.timer 2> /dev/null");
+		}
+		// Make a readable copy of allmon3.ini (Allmon3 updates can reset the file permissions)
+		$fname = '/etc/allmon3/allmon3.ini';
+		if(file_exists($fname)) {
+			$fname2 = '/etc/asterisk/allmon.ini.php';
+			if(exec("diff $fname $fname2"))
+				execCmd("cp $fname $fname2");
+			if((fileperms($fname2) & 0777) != 0660)
+				execCmd("chmod 660 $fname2");
+			if(getGroupName($fname2) !== $group)
+				execCmd("chgrp $group $fname2");
+		}
 	}
 }
 
@@ -226,7 +235,39 @@ if($fn) {
 		restartWebServer();
 	}
 } else {
-	msg("WARNING: php.ini not found in /etc/php/");
+	msg("php.ini not found in /etc/php/");
+}
+
+// Copy DTMF command script and audio files to Asterisk folder
+msg("Copy DTMF command support files to /usr/share/asterisk/agi-bin/?");
+$s = readline("Enter 'y' to confirm, any other key to skip: ");
+if($s === 'y') {
+	$d0 = "$webdir/$asdir/_tools/agi-bin";
+	$d1 = "/usr/share/asterisk/agi-bin";
+	$ls0 = trim(shell_exec("ls $d0"));
+	if(!$ls0) {
+		msg("Error reading $d0/.");
+	} else {
+		if(!file_exists($d1)) {
+			msg("$d1/ not found.");
+		} else {
+			$f0 = explode(NL, $ls0);
+			$ls1 = trim(shell_exec("ls $d1"));
+			$f1 = $ls1 ? explode(NL, $ls1) : [];
+			$fcp = [];
+			foreach($f0 as $f) {
+				if(array_search($f, $f1) === false || exec("diff $d0/$f $d1/$f"))
+					$fcp[] = $f;
+			}
+			if(!count($fcp)) {
+				msg("Files already present.");
+			} else {
+				foreach($fcp as $f)
+					execCmd("cp $d0/$f $d1/$f");
+			}
+		}
+		msg("See $webdir/$asdir/docs/rpt.conf and extensions.conf for DTMF command setup notes.");
+	}
 }
 
 msg("Install/Update Complete.");
@@ -266,20 +307,23 @@ if(preg_match('/bindport = ([0-9]{4,5})/', $bp, $m) == 1) {
 msg("AllScan can be accessed at:\n\t$lip on the local network, or\n"
 	."\t$wip remotely\n\tif your router has port $port forwarded to this node.");
 
-msg("Be sure to bookmark the above URL(s) in your browser.");
-msg("IMPORTANT: After updates do a CTRL-F5 in your browser (or long-press the reload button in mobile browsers), or clear the browser cache, so that all CSS and JavaScript files will properly update.");
+if($dlfiles) {
+	msg("Be sure to bookmark the above URL(s) in your browser.");
+	msg("IMPORTANT: After updates do a CTRL-F5 in your browser (or long-press the reload button in mobile\n" 
+		."browsers), or clear the browser cache, so that CSS and JavaScript files will properly update.");
+}
 
 exit();
 
 // ---------------------------------------------------
 // Execute command, show the command, show the output and return val
 function execCmd($cmd) {
-	msg("Executing cmd: $cmd");
+	echo "Executing cmd: $cmd ... ";
 	$out = '';
 	$res = 0;
 	$ok = (exec($cmd, $out, $res) !== false && !$res);
 	$s = $ok ? 'OK' : 'ERROR';
-	msg("Return Code: $s");
+	msg($s);
 	return $ok;
 }
 
@@ -342,6 +386,13 @@ function restartWebServer() {
 		$cmd = "systemctl restart lighttpd.service 2> /dev/null";
 	if(!execCmd($cmd))
 		msg("Restart webserver or restart node now");
+}
+
+function getGroupName($f) {
+	if(!($id = filegroup($f)))
+		return '';
+	$a = posix_getgrgid($id);
+	return $a['name'] ?? '';
 }
 
 function msg($s) {
